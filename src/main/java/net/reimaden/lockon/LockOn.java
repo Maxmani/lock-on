e224713802143ca2017aa8c1c1a94514ffff99cf
Maxmani <mikael.hannukainen@gmail.com>
2023-10-22 23:53:25 +0300

    Remove unused strings

--- src/main/java/net/reimaden/lockon/LockOn.java
+++ src/main/java/net/reimaden/lockon/LockOn.java
@@ -4,14 +4,10 @@ import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
 import net.reimaden.lockon.config.LockOnConfig;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class LockOn implements ClientModInitializer {
 
 	public static final String MOD_ID = "lockon";
-	public static final String MOD_NAME = "Lock On";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
 	
 	@Override
 	public void onInitializeClient() {
